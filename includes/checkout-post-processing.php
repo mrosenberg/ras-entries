9dd1983236cb157839d532f0f74d3dc04f68fdfe
Matthew Rosenberg <matt@matthewrosenberg.com>
2017-01-10 12:30:03 -0500

    add support for student entries and IPN ping back

--- includes/checkout-post-processing.php
+++ includes/checkout-post-processing.php
@@ -4,39 +4,32 @@
 class RAS_Post_Checkout_Processing {
 
 
-
 	public function __construct() {
 
-		add_action( 'gform_paypal_fulfillment', array( $this, 'process_order' ), 10, 4 );
+		add_action( 'gform_paypal_post_ipn', array( $this, 'process_order' ), 10, 4 );
 	}
 
 
-	public function process_order( $paypal_entry, $feed, $transaction_id, $amount ) {
-		$user     = wp_get_current_user();		
-		$filter   = array();
-	    $order_id = rgar( $paypal_entry, 'id' );
+	public function process_order( $paypal_ping, $order_entry, $cancel ) {
+		$user       = wp_get_current_user();		
+		$order      = GFAPI::get_entry( $order_entry );
+		$entries    = rgar( $order, 9 ); 
+		$entry_ids  = explode( ',', $entries );
+		$txn_id     = $paypal_ping[ 'txn_id' ];
+		$txn_status = $paypal_ping[ 'payment_status' ];
+		$txn_date   = $paypal_ping[ 'payment_date' ];
 
 
-		$filter[ 'status' ]          = 'active';
-		$filter[ 'field_filters' ][] = array(
-			'key'   => 'created_by', 
-			'value' => $user->ID				
-		);
-		$filter[ 'field_filters' ][] = array(
-			'key'   => 7, 
-			'value' => 'open'		
-		);		
+		foreach ( $entry_ids as $id ) :
 
-		$entries = GFAPI::get_entries( 0, $filter );
+			$entry = GFAPI::get_entry( $id );
 
-		foreach( $entries as $entry ) {
-			$timestamp = new DateTime();
-
-			GFAPI::update_entry_property( $entry['id'], 7, 'closed' );
-			GFAPI::update_entry_property( $entry['id'], 'trasaction_id', $transaction_id );
-			GFAPI::update_entry_property( $entry['id'], 'payment_status', 'Approved' );
-			GFAPI::update_entry_property( $entry['id'], 'payment_date', $timestamp->format( 'c' ) );
-		}
+			GFAPI::update_entry_property( $entry['id'], 7,                'closed'    );
+			GFAPI::update_entry_property( $entry['id'], 'trasaction_id',  $txn_id     );
+			GFAPI::update_entry_property( $entry['id'], 'payment_status', $txn_status );
+			GFAPI::update_entry_property( $entry['id'], 'payment_date',   $txn_date   );	
+					
+		endforeach;
 	}
 
 }
--- includes/form-prefills.php
+++ includes/form-prefills.php
@@ -8,16 +8,17 @@ class RAS_GF_Prefills {
 	public function __construct() {
 
 		add_filter( 'gform_field_value_company_name',        array( $this, 'company_name_pre_populate'           ) );
-		add_filter( 'gform_field_value_checkout_entry_ids',  array( $this, 'checkout_cart_entry_ids_prepopulate' ) );		
+		add_filter( 'gform_field_value_checkout_entry_ids',  array( $this, 'checkout_cart_entry_ids_prepopulate' ) );
 		add_filter( 'gform_field_value_checkout_cart_total', array( $this, 'checkout_cart_total_pre_populate'    ) );
+    add_filter( 'gform_field_value_checkout_do_paypal',  array( $this, 'checkout_cart_do_paypal_flag'        ) );
 	}
 
 
 	/**
 	 ** Prefill the company name from when the user registered
-	 ** @public 
+	 ** @public
 	 ** @return string
-	**/ 
+	**/
 	public function company_name_pre_populate( $value ) {
 		$user    = wp_get_current_user();
 		$company = get_user_meta( $user->ID, 'company_name', true );
@@ -31,7 +32,7 @@ class RAS_GF_Prefills {
 
 		$carry += $item;
 		return $carry;
-	} 
+	}
 
 
 	private function open_entries() {
@@ -41,14 +42,14 @@ class RAS_GF_Prefills {
 
 		$filter[ 'status' ]        = 'active';
 		$filter[ 'field_filters' ] = array(
-			array( 
-				'key'   => 'created_by', 
-				'value' => $user->ID				
+			array(
+				'key'   => 'created_by',
+				'value' => $user->ID
 			),
-			array( 
-				'key'   => 7, 
+			array(
+				'key'   => 7,
 				'value' => 'open'
-			)			
+			)
 		);
 
 		return GFAPI::get_entries( 0, $filter );
@@ -68,24 +69,51 @@ class RAS_GF_Prefills {
 	}
 
 
+
+  /**
+   ** Check if user is student elligbile for reduced rate
+   ** @private
+   ** @return bool
+  **/
+  private function is_student() {
+    $user = wp_get_current_user();
+    $type = get_user_meta( $user->ID, 'registration_type', true );
+
+    return 'student' === $type;
+  }
+
 	/**
 	 ** Fill in the proper cart total before checkout
-	 ** @public 
+	 ** @public
 	 ** @return string
-	**/ 
-	public function checkout_cart_total_pre_populate( $value ) {
-		$entries = $this->open_entries();
-
+	**/
+	public function checkout_cart_total_pre_populate() {
+    $is_student = $this->is_student();
+		$entries    = $this->open_entries();
+    $purchases  = array();
 
 		foreach ( $entries as $entry ) {
 			$purchases[] = $entry[6];
 		}
 
+    asort( $purchases );
+
+    if( $is_student ) {
+
+      $purchases = array_slice( $purchases, 3 );
+    }
+
 		$purchase_total  = array_reduce( $purchases, array( $this, 'purchase_total' ), 0 );
 
 		return $purchase_total;
-	}	
+	}
 
+
+  public function checkout_cart_do_paypal_flag() {
+    $total = $this->checkout_cart_total_pre_populate();
+
+    return $total > 0 ? 1 : 0;
+  }
 }
 
 
