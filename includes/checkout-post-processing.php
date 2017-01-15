d91ef8c6e07debebc802728a18295f3df6ea123c
Matthew Rosenberg <matt@matthewrosenberg.com>
2017-01-15 11:27:38 -0500

    immediately close entries upon submission

--- includes/checkout-post-processing.php
+++ includes/checkout-post-processing.php
@@ -6,14 +6,29 @@ class RAS_Post_Checkout_Processing {
 
 	public function __construct() {
 
-		add_action( 'gform_paypal_post_ipn', array( $this, 'process_order' ), 10, 4 );
+    add_action( 'gform_after_submission_11', array( $this, 'close_entries' ), 10, 2 );
+		add_action( 'gform_paypal_post_ipn',     array( $this, 'process_order' ), 10, 4 );
 	}
 
 
+  public function close_entries( $order_entry, $form ) {
+    $order     = GFAPI::get_entry( $order_entry );
+    $entries   = rgar( $order, 10 );
+    $entry_ids = explode( ',', $entries );
+
+    foreach ( $entry_ids as $id ) :
+
+      $entry = GFAPI::get_entry( $id );
+
+      GFAPI::update_entry_field( $entry['id'], 7, 'closed' );
+
+    endforeach;
+  }
+
+
 	public function process_order( $paypal_ping, $order_entry, $cancel ) {
-		$user       = wp_get_current_user();		
 		$order      = GFAPI::get_entry( $order_entry );
-		$entries    = rgar( $order, 9 ); 
+		$entries    = rgar( $order, 10 );
 		$entry_ids  = explode( ',', $entries );
 		$txn_id     = $paypal_ping[ 'txn_id' ];
 		$txn_status = $paypal_ping[ 'payment_status' ];
@@ -24,11 +39,10 @@ class RAS_Post_Checkout_Processing {
 
 			$entry = GFAPI::get_entry( $id );
 
-			GFAPI::update_entry_property( $entry['id'], 7,                'closed'    );
-			GFAPI::update_entry_property( $entry['id'], 'trasaction_id',  $txn_id     );
-			GFAPI::update_entry_property( $entry['id'], 'payment_status', $txn_status );
-			GFAPI::update_entry_property( $entry['id'], 'payment_date',   $txn_date   );	
-					
+			GFAPI::update_entry_field( $entry['id'], 'trasaction_id',  $txn_id     );
+			GFAPI::update_entry_field( $entry['id'], 'payment_status', $txn_status );
+			GFAPI::update_entry_field( $entry['id'], 'payment_date',   $txn_date   );
+
 		endforeach;
 	}
 
